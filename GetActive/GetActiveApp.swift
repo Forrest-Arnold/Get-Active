cb40f991ebba3f1facbcf8accdbb96cdd52b7b50
Forrest-Arnold <forkalarn@gmail.com>
2025-04-01 22:39:34 -0700

    Initial Commit

---
+++ GetActive/GetActiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetActiveApp.swift
+//  GetActive
+//
+//  Created by Forrest Kalani Arnold on 4/1/25.
+//
+
+import SwiftUI
+
+@main
+struct GetActiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GetActiveTests/GetActiveTests.swift
@@ -0,0 +1,16 @@
+//
+//  GetActiveTests.swift
+//  GetActiveTests
+//
+//  Created by Forrest Kalani Arnold on 4/1/25.
+//
+
+import Testing
+
+struct GetActiveTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
